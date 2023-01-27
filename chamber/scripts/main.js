b38e8d606ee0fc85e6d6aa96e488856111d47599
juliaritchie <jujaritchie@gmail.com>
2023-01-27 11:11:09 -0700

    commit to master

---
+++ chamber/scripts/main.js
@@ -0,0 +1,39 @@
+const datefield = document.querySelector(".date");
+const datefieldUK = document.querySelector("aside"); // for european/family history format with day first.
+
+// derive the current date using a date object
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+	now
+);
+const fulldateUK = new Intl.DateTimeFormat("en-UK", {
+	dateStyle: "full"
+}).format(now);
+// long, medium, short options ... try them
+const button = document.querySelector('.btn');
+
+function show() {
+    // button.classList.add('newstyle');
+    button.classList.toggle('newstyle');
+}
+
+button.addEventListener('click', show );
+
+
+// Hamburger Button JS
+
+const mainnav = document.querySelector('.navigation');
+const hambutton = document.querySelector('.ham');
+hambutton.addEventListener('click', () => {
+    mainnav.classList.toggle('responsive');
+})
+
+datefield.innerHTML = `<em>${fulldate}</em>`;
+let date = new Date();
+let year = date.getFullYear();
+
+document.querySelector('.year').textContent = year;
+
+
+let currentdate = new Date(document.lastModified);
+document.querySelector('.currentdate').textContent = currentdate;
