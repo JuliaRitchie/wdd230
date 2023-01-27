0341edd4b8fb971044b4e2af6b9fbce10dd79528
juliaritchie <jujaritchie@gmail.com>
2023-01-27 09:32:28 -0700

    commit to master

--- test/scripts/week4.js
+++ test/scripts/week4.js
@@ -1,7 +1,17 @@
 const button = document.querySelector('.btn');
 
 function show() {
-    button.classList.add('newstyle');
+    // button.classList.add('newstyle');
+    button.classList.toggle('newstyle');
 }
 
 button.addEventListener('click', show );
+
+
+// Hamburger Button JS
+
+const mainnav = document.querySelector('.navigation');
+const hambutton = document.querySelector('.ham');
+hambutton.addEventListener('click', () => {
+    mainnav.classList.toggle('responsive');
+})
