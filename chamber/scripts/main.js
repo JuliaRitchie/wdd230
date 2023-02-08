da38b7394e005f10f2d97e495ac25338633ec13f
juliaritchie <jujaritchie@gmail.com>
2023-02-08 13:30:52 -0700

    commit to master

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -36,4 +36,15 @@ if (weekday == 1 || weekday == 2){
 	document.querySelector('.dynamic').textContent = 'Come to the Wednesday Chamber of Commerce meet and greet!'
 }
 
-// windchilll calculation
+// windchill calculation
+
+const temp = document.querySelector('.t').textContent;
+
+const windspeed = document.querySelector('.ws').textContent;
+let chill = Math.round((35.74 + (0.6215 * temp))-(35.75 * Math.pow(windspeed,0.16)) + (0.4275*temp*Math.pow(windspeed,0.16)));
+
+let windchill = document.querySelector('.wc');
+
+if (temp <= 50 && windspeed >= 3){
+	windchill.textContent = chill + ' mph';
+}
