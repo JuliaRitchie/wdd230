29dbe9c1d25029f73fc208b297b7a8de05774cff
juliaritchie <jujaritchie@gmail.com>
2023-03-04 20:08:35 -0700

    commit to master

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,4 +1,19 @@
 const url = 'https://juliaritchie.github.io/wdd230/chamber/directory.json'
+
+const card_view = document.querySelector('#cards')
+const list_view = document.querySelector('#list')
+card_view.addEventListener('click', () => {
+    async function getBusinessData() {
+        const response = await fetch(url);
+        const data = await response.json();
+        console.table(data.businesses);  // note that we reference the prophet array of the data object given the structure of the json file
+        displayBusiness(data.businesses);
+      }
+    getBusinessData();})
+list_view.addEventListener('click', () => {
+    mainnav.classList.toggle('responsive');
+})
+
 function showCards(){
     let row = document.querySelectorAll('tr');
     row.forEach((item) => {
@@ -26,21 +41,24 @@ const displayBusiness = (businesses) => {
       let card = document.createElement('section');
       let h2 = document.createElement('h2');
       let portrait = document.createElement('img');
+      let p = document.createElement('p');
+      let p2 = document.createElement('p');
   
       // Build the h2 content out to show the prophet's full name - finish the template string
       h2.textContent = `${business.name}`;
-     
+      p.textContent = `Address: ${business.address}`
+      p2.textContent = `Website: ${business.website}`
   
       // Build the image portrait by setting all the relevant attribute
       portrait.setAttribute('src', business.imageurl);
       portrait.setAttribute('alt', `Logo of ${business.name}`);
       portrait.setAttribute('loading', 'lazy');
-      portrait.setAttribute('width', '340');
-      portrait.setAttribute('height', '440');
   
       // Append the section(card) with the created elements
       card.appendChild(h2);
       card.appendChild(portrait);
+      card.appendChild(p);
+      card.appendChild(p2);
   
       cards.appendChild(card);
     }) // end of forEach loop
@@ -48,24 +66,24 @@ const displayBusiness = (businesses) => {
 
 
 
-function displayTable(prophets){
+function displayTable(businesses){
     let row = document.querySelectorAll('tr');
     row.forEach((item) =>{
     item.remove();})
     
-    prophets.forEach((prophet) => {
+    businesses.forEach((business) => {
         let tr = document.createElement('tr');
         let td_name = document.createElement('td');
-        let td_birthplace = document.createElement('td');
-        let td_birthdate = document.createElement('td');
+        let td_address = document.createElement('td');
+        let td_website = document.createElement('td');
 
-        td_name.textContent = `${prophet.name} ${prophet.lastname}`;
-        td_birthplace.textContent = prophet.birthplace;
-        td_birthdate.textContent = prophet.birthdate;
+        td_name.textContent = `${business.name}`;
+        td_address.textContent = business.address;
+        td_website.textContent = business.website;
 
         tr.appendChild(td_name);
-        tr.appendChild(td_birthplace);
-        tr.appendChild(td_birthdate);
+        tr.appendChild(td_address);
+        tr.appendChild(td_website);
         document.querySelector('table').appendChild(tr);
     })
 }
